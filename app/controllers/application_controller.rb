2abe483c6c8e408c392830e4e887a50c27e21042
YaKonsatantin <jakonstantin@gmail.com>
2019-06-14 16:27:06 +0300

    russification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,23 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authorize
+  before_action :set_i18n_locale_from_params
 # ...
-
   protected
-
-  def authorize
-   unless User.find_by(id: session[:user_id])
-   redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+   def set_i18n_locale_from_params
+     if params[:locale]
+     if I18n.available_locales.map(&:to_s).include?(params[:locale])
+     I18n.locale = params[:locale]
+     else
+         flash.now[:notice] =
+               "#{params[:locale]} translation not available"
+         # перевод недоступен
+         logger.error flash.now[:notice]
+         end
    end
  end
+ 
+def default_url_options
+   { locale: I18n.locale }
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
